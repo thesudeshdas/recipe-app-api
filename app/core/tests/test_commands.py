2f1874bd0bace1c850ebcb3960bbf6082a04fd03
thesudeshdas <sudeshkumardas7gmail.com>
2023-08-31 17:27:17 +0530

    fix: docker settings

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,7 +4,7 @@ Test custom Django management commands
 
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg20pError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -24,9 +24,9 @@ class CommandTests(SimpleTestCase):
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def wait_for_db_delay(self, patched_sleep, patched_check):
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError"""
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
+        patched_check.side_effect = [Psycopg20pError] * 2 + \
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
