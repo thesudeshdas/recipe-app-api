dd643a92f518a60e0d5c5939757e22c9aa21d296
thesudeshdas <sudeshkumardas7gmail.com>
2023-09-06 17:20:56 +0530

    fix create user api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -33,7 +33,7 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_202_CREATED)
-        user = get_user_model().objects().get(email=payload['email'])
+        user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
@@ -53,7 +53,7 @@ class PublicUserApiTests(TestCase):
         "Test if the password is less than 5 chars"
         payload = {
             'email': 'test@example.com',
-            'password': 'test',
+            'password': 'pw',
             'name': 'Test Name'
         }
 
@@ -61,7 +61,7 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects().filter(
-            email=payload["email"]
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
